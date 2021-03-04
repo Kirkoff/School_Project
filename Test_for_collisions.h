7e0e5f46eaf9c584d3ac51c7c4c660f6f0830a9f
Kirkoff <66189929+Kirkoff@users.noreply.github.com>
2021-03-04 23:19:23 +0300

    Update and rename Test_for_collisions.hpp to Test_for_collisions.h

---
+++ Test_for_collisions.h
@@ -0,0 +1,16 @@
+//
+//  Test_for_collisions.h
+//  Rolling Hash
+//
+//  Created by Kirill on 24.02.2021.
+//
+
+#pragma once
+#ifndef Test_for_collisions_h
+#define Test_for_collisions_h
+
+#include <stdio.h>
+#include <map>
+#endif /* Test_for_collisions_h */
+
+void count_collisions(std::map<char, int> values, int MOD, int prime_number, int& result);
