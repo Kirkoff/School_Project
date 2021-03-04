faa2ab6e64b1f4aec80af30751bc068c3dc1e764
Kirkoff <66189929+Kirkoff@users.noreply.github.com>
2021-03-04 23:19:02 +0300

    Update Test_for_collisions.cpp

--- Test_for_collisions.cpp
+++ Test_for_collisions.cpp
@@ -4,25 +4,27 @@
 //
 #pragma once
 #include <unordered_set>
-#include "Test_for_collisions.hpp"
-#include "Gen.hpp"
-#include "HashFucntion.hpp"
+#include "Test_for_collisions.h"
+#include "Gen.h"
+#include "HashFunction.h"
 
-int count_collisions(std::map<char, int> values, int MOD, int prime_number){
+void count_collisions(std::map<char, int> values, int MOD, int prime_number, int& result) {
     int cnt = 0;
     std::unordered_multiset<int> hashes;
-    for(int i = 0; i < 1e6; ++i){
+    for (int i = 0; i < 1e6; ++i) {
         //сгенерировать строку
-        std::string cur_s = generate_string(16, 1e9 + 7); //1e9 + 7 рандомный seed
+        std::string cur_s = generate_string(10, 1e9 + 7); //1e9 + 7 рандомный seed
         //посчитать хэш-функцию
         int cur_hash = get_hash(cur_s, values, MOD, prime_number);
         //проверить на коллизию
-        if (hashes.find(cur_hash) == hashes.end()){
+        if (hashes.find(cur_hash) == hashes.end()) {
             hashes.insert(cur_hash);
-        }else{
+        }
+        else {
             cnt += hashes.count(cur_hash);
             hashes.insert(cur_hash);
         }
     }
-    return cnt;
+    result = cnt;
+    return;
 }
